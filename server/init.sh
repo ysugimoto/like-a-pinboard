423c4a9afb25d13b26e187ad24359048da53539c
sugimoto <sugimoto@wnotes.net>
2014-12-16 00:55:19 +0900

    Renamed server to api

--- server/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export GOPATH=`pwd`
-echo "Setting GOPATH to ${GOPATH}."
-
-echo "Getting dependency packages..."
-go get -u github.com/ysugimoto/husky
-go get -u github.com/go-sql-driver/mysql
